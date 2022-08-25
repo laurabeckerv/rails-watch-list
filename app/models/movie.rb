5bb653c1e84f665e9ebca172e830c66a316c54e3
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-08-25 12:18:16 +0200

    model, attributes, validations added

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+# Associations
+# You can’t delete a movie if it is referenced in at least one bookmark.
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :lists do
+    resources :reviews, only: [:new, :create]
+  end
 end
